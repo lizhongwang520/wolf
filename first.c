9f32f15197ee8821cd973eeda6cff70970b83d9b
lizhongwang520 <2506756358@qq.com>
2017-09-23 13:45:36 +0800

    the file contants a function

---
+++ first.c
@@ -0,0 +1,11 @@
+/*this is the first file for me.
+i want to learn hph,html,python,c++,C and so on
+*/
+/*this is the first function*/
+int ReturnMaxOne(int a,intb)
+{
+	if(a>b)
+		return a;
+	else
+		return b;
+}
